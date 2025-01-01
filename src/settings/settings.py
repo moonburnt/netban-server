3b1af8c668eb508f43cbdc34d34a2a49f50c4c08
moonburnt <moonburnt@disroot.org>
2025-01-02 00:30:43 +0300

    refactor: rearrange django internals

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "app.urls"
+ROOT_URLCONF = "urls"
 
 TEMPLATES = [
     {
@@ -54,7 +54,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app.wsgi.application"
+WSGI_APPLICATION = "wsgi.application"
 
 
 # Database
